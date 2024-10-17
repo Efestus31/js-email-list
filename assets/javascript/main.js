5ef17d2f3906742b887b6e3619eca328269bb929
Efestus31 <biagiohd@gmail.com>
2024-10-17 15:04:46 +0200

    add const per elemento nella DOM piu' resa problema in mini problemi

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+const emailList = document.getElementById('email');
+// dichiaro la costante email preso l'id all'interno della DOM
+
+
+ //ciclo per creare 10 email
+//fetch invia richiesta API
+//creaiamo un nuovo elemento list item e lo aggiungiamo alla lista
+      
