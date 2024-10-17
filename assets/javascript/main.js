764de51f8e0e3e8b14a15408ede8c84b6eab2edf
Efestus31 <biagiohd@gmail.com>
2024-10-17 15:53:42 +0200

    finzione e ciclo per recuperare le mail dall'api

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,27 @@ const emailList = document.getElementById('email');
 // dichiaro la costante email preso l'id all'interno della DOM
 
 
- //ciclo per creare 10 email
-//fetch invia richiesta API
-//creaiamo un nuovo elemento list item e lo aggiungiamo alla lista
-      
+const fetchemail = () => {
+
+    
+
+    for(let i = 0; i < 10; i++){
+        //ciclo per creare 10 email
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        //fetch invia richiesta API
+        .then(response => response.json())
+        .then(data => {
+            //creaiamo un nuovo elemento list item e lo aggiungiamo alla lista
+            const email = data.response;
+            const listItem = document.createElement('li');
+            listItem.textContent = email;
+            emailList.appendChild(listItem);
+        })
+    }
+}
+
+
+
+
+fetchemail();
+
